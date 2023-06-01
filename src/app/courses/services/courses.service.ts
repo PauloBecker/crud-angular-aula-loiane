482d669ca2139e0727a8e390132ea079d3f1249d
Paulo Becker <beckerpaulo81@gmail.com>
2023-06-01 09:03:22 -0300

    Aula 29: Atualizar curso & salvando formulário - refatorarando a classe Curso Service do Angular para tratar a lógica de criar (HTTP POST) e atualizar (HTTP PUT) um registro.

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -26,10 +26,28 @@ export class CoursesService {
     return this.httpClient.get<Course>(`${this.API}/${id}`);
   }
 
+
+
   save(record: Partial<Course>){
+    console.log(record);
 
+    if (record._id) {
+      console.log('update');
+
+      return this.update(record);
+    }
+    console.log('create');
+
+    return this.create(record);
+
+  }
+
+  private create(record: Partial<Course>){
     return this.httpClient.post<Course>(this.API,record);
+  }
 
+  private update(record: Partial<Course>){
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record);
   }
 
 }
